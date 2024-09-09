5d5fa4d90970d81a14a3df290b693f4b3432d345
Callie Lunt <callielunt@MacBook-Pro-543.app.byu.edu>
2024-09-09 13:45:14 -0600

    EDA added

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,20 @@
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+library(DataExplorer)
+
+# Read in Data
+sampleSubmission <- vroom("sampleSubmission.csv")
+test <- vroom("test.csv")
+train <- vroom("train.csv")
+
+# Perform EDA
+glimpse(train)
+plot_intro(train)
+plot_correlation(train)
+plot_bar(train)
+plot_histogram(train)
+plot_missing(train)
+
+# Weather Barplot
+weather <- ggplot(data = train, mapping = aes(x = weather, y = count)) +
