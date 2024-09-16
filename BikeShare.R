2ebf6399eb1424d4f39531a829533748321943f5
Callie Lunt <callielunt@MacBook-Pro-543.local>
2024-09-16 11:46:44 -0600

    Added Feature Engineering

--- BikeShare.R
+++ BikeShare.R
@@ -134,3 +134,58 @@ kaggle_submission_pois
 
 # Write out the file to submit to Kaggle
 vroom_write(x= kaggle_submission_pois, file = "./PoisPreds1.csv", delim = ",")
+
+
+# Recipe Stuff and Feature Engineering
+
+# Cleaning
+new_train <- train |> select(-casual, -registered) |> # removes casual and registered
+  mutate("log_count"= log(count)) |> # makes variable log_count 
+  select(-count) # removes variable count 
+
+# Feature Engineering
+bike_recipe <- recipe(log_count ~ ., data = new_train) %>% 
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% # changes weather 4 into a 3
+  step_mutate(weather = factor(weather, levels = c(1,2,3), 
+                               labels = c("clear", "cloudy", "rainy"))) %>%  # sets weather into a factor
+  step_time(datetime, features = "hour") %>% # extracts hour variable from timestamp
+  step_mutate(datetime_hour = factor(datetime_hour)) %>% # makes hour a factor
+  step_mutate(season = factor(season, levels = c(1,2,3,4), 
+                              labels = c("spring", "summer", "fall", "winter"))) %>% # makes season a factor
+  step_dummy(all_nominal_predictors()) %>% # creates dummy variables %>% 
+  step_rm(datetime)
+  
+
+# making sure it did what I wanted
+# prepped_recipe <- prep(bike_recipe)
+# final <- bake(prepped_recipe, new_data = new_train)
+# glimpse(final)
+
+# Define a Model
+lin_model <- linear_reg() %>% 
+  set_engine("lm") %>% 
+  set_mode("regression")
+
+bike_workflow <- workflow() %>% 
+  add_recipe(bike_recipe) %>% 
+  add_model(lin_model) %>% 
+  fit(data = new_train)
+
+# Run all the Steps on test data
+lin_preds <- predict(bike_workflow, new_data = test)
+
+submission <- lin_preds |>
+  bind_cols(test) |> # bind preditions with test data
+  select(datetime, .pred) |> # just keep datetime and prediction value
+  rename(count = .pred) |> #rename pred to count as Kaggle submission wants
+  mutate(count = exp(count)) |> # make count not log_count
+  mutate(count = pmax(0, count)) |> # pointwise max of (0, prediciton) ie if prediction is <0 make 0
+  mutate(datetime = as.character(format(datetime))) # needed fo right format to Kaggle
+
+submission
+
+# Write out the file to submit to Kaggle
+vroom_write(x= submission, file = "./LinearPreds2.csv", delim = ",")
+
+
+
